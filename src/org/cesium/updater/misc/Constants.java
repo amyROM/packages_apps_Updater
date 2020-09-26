a157e429129f0a2a4346597ec1847689f431ac87
SahilSonar <sss.sonar2003@gmail.com>
2020-09-26 12:14:41 +0530

    Updater: Update hardcoded strings for eleven
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/org/cesium/updater/misc/Constants.java
+++ src/org/cesium/updater/misc/Constants.java
@@ -41,7 +41,7 @@ public final class Constants {
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "org.cesium.build_version";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "org.cesium.device";
+    public static final String PROP_DEVICE = "ro.cesium.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_RELEASE_TYPE = "org.cesium.build_type";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "cesium.updater.allow_downgrading";
