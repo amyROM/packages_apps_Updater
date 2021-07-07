be6a7c48915bdd635c2930f99b4f91ff6407916d
SahilSonar <sss.sonar2003@gmail.com>
2021-07-07 12:28:20 +0200

    Updater: Refactor strings & props for amyROM
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/org/cesium/updater/misc/Constants.java
+++ src/org/cesium/updater/misc/Constants.java
@@ -39,13 +39,13 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "org.cesium.build_version";
+    public static final String PROP_BUILD_VERSION = "ro.lineage.version";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.cesium.device";
+    public static final String PROP_DEVICE = "ro.product.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "org.cesium.build_type";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "cesium.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "org.cesium.updater.uri";
+    public static final String PROP_RELEASE_TYPE = "ro.lineage.releasetype";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "ro.lineage.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "ro.lineage.updater.uri";
     public static final String PROP_RECOVERY_UPDATE = "persist.sys.recovery_update";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
